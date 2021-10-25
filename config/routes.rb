04385a7566c217f8edcf38929f6e83f818f86b4b
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-10-25 18:52:08 +0530

    Create sign-in functionality without sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/", to: "home#index"
-  post "users/login", to: "users#login"
+  #post "users/login", to: "users#login"
   resources :todos, :users
+
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
