fecf3682cf015574f9fa852be929707cfbe5c632
rajkumarsamanta <rajkumar.samanta@bcrec.ac.in>
2021-10-27 19:34:47 +0530

    Skip before action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,6 @@ class UsersController < ApplicationController
     render "users/new"
   end
 
-  # def index
-  #   render plain: User.all.to_displayable_list.join("\n")
-  # end
-
-  # def show
-  #   id = params[:id]
-  #   user_id = User.find(id)
-  #   render plain: user_id.to_displayable_string
-  # end
-
   def create
     first_name = params[:first_name]
     last_name = params[:last_name]
@@ -35,22 +25,4 @@ class UsersController < ApplicationController
       redirect_to "/"
     end
   end
-
-  # def update
-  #   id = params[:id]
-  #   email = params[:email]
-  #   password = params[:password]
-  #   user = User.find(id)
-  #   user.email = email
-  #   user.password = password
-  #   user.save!
-  #   render plain: "Updated user email and password !"
-  # end
-
-  # def login
-  #   user_email = params[:email]
-  #   passwd = params[:password]
-  #   user = User.find_by(email: user_email, password: passwd)
-  #   render plain: "User login status : #{user.presence?}"
-  # end
 end
